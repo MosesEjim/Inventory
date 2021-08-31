d5ae88ee67b25d498bc9ff55bedb405510a6532d
mosesejim <mosesejim@outlook.com>
2021-08-31 03:25:46 +0100

    added seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'=>'admin',
+            'password'=>bcrypt('secret'),
+            'email'=>'admin@gmail.com'
+        ]);
+    }
+}
